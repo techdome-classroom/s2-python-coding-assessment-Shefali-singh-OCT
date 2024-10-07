5bacb5e52b082ae9965cf3fad6e4604a273158d8
Shefali singh <146179686+Shefali-singh-OCT@users.noreply.github.com>
2024-10-07 14:44:25 +0530

    Add files via upload

--- program1.py
+++ program1.py
@@ -1,10 +1,16 @@
 class Solution(object):
-    def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        pass
+    def isValid(self, s: str) -> bool:
+         stack = []
+         matching = {')' : '(','[':']','{':'}'}
+         for char in s:
+             if char in matching:
+                  if not stack or stack.pop() != matching[char]:
+                      return False
+                  else:
+                      stack.append(char) 
+         return not stack
+                  
+    
 
 
 
--- program2.py
+++ program2.py
@@ -1,9 +1,13 @@
 class Solution(object):
-    def romanToInt(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
+    def romanToInt(self, num: int) -> str:
+        val = [1000,900,500,400,100,90,50,40,10,9,5,4,1]
+        roman = ["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"]
+        result = ""
+        for i in range(len(val)):
+            while num >= range(len(val)):
+                result += roman[i]
+                num -= val[i] 
+        return result          
         pass
 
 
